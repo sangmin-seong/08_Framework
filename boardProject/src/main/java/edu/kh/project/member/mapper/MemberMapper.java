a39da9e9b23d26d73dad545c39852f759b4898b9
sangmin-seong <money679@naver.com>
2024-09-11 16:28:57 +0900

    20240911_수업내용(홈페이지 만들기[로그인, 로그아웃, 쿠키 저장])

---
+++ boardProject/src/main/java/edu/kh/project/member/mapper/MemberMapper.java
@@ -0,0 +1,16 @@
+package edu.kh.project.member.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import edu.kh.project.member.dto.Member;
+
+@Mapper
+public interface MemberMapper {
+
+	/** memberEmail이 일치하는 회원 정보 조회
+	 * @param memberEmail
+	 * @return loginMember / null
+	 */
+	Member login(String memberEmail);
+	
+}
